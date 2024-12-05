360961e9c72cb1322eadaf463071c01381303415
Thim Eickhof <thim.eickhof@hotmail.nl>
2024-12-05 21:19:39 +0100

    Day 5 start

---
+++ 5/main.js
@@ -0,0 +1,18 @@
+import fetchInput from "../fetch-input.mjs";
+
+// Input
+const input = await fetchInput(2024, 5);
+
+function processInput(input) {
+
+}
+
+// Helpers
+
+
+// Part 1
+
+
+// Part 2
+
+
